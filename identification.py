3ac239b8d4c1930f256194ca27ef2309fa40bf5f
rayaneDZ <chlef-algerie@hotmail.fr>
2019-06-26 14:12:51 +0100

    adjusted identification.py to use picamera

--- identification.py
+++ identification.py
@@ -3,23 +3,22 @@ import numpy as np
 import pickle
 import RPi.GPIO as io
 import time
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+print("Face Identification Program is running...")
 
 io.setmode(io.BOARD)
-
-print("hey the face identification program is running")
 
 red = 7
 green = 11
 servo = 13
-buzzer = 12
 
-io.setup(buzzer, io.OUT)
 io.setup(red, io.OUT)
 io.setup(green, io.OUT)
 io.setup(servo, io.OUT)
 
 p_servo = io.PWM(servo, 50)
-p_buzzer = io.PWM(buzzer, 50)
 
 io.output(red, 1)
 io.output(green, 0)
@@ -33,7 +32,10 @@ with open("labels.pickle", "rb") as f:
     labels = pickle.load(f)
     labels = {v: k for k, v in labels.items()}
 
-cap = cv2.VideoCapture(0)
+camera = PiCamera()
+camera.ISO = 800
+camera.resolution = (640, 480)
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
 times = 0
 done = False
@@ -43,8 +45,16 @@ while True:
     if done:
         break
 
-    ret, img = cap.read()
+    #take a picture
+    camera.capture(rawCapture, format="bgr")
+    
+    #img is a numpy array
+    img = rawCapture.array
+    
+    #turn it to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    #Detect the face
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x, y, w, h) in faces:
@@ -54,33 +64,47 @@ while True:
 
         if conf >= 50:
             name = labels[id_]
-            print(name)
-            #font = cv2.FONT_HERSHEY_SIMPLEX
-            #color = (0, 255, 0)
-            #stroke = 2
-            #cv2.putText(img, name, (x, (y - 10)), font, 1,color, stroke, cv2.LINE_AA)
-            #if name == "rayane":
-            times = times + 1
+            print(name, conf)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            color = (0, 255, 0)
+            stroke = 2
+            
+            #write name on img
+            cv2.putText(img, name, (x, (y - 10)), font, 1,color, stroke, cv2.LINE_AA)
+            
+            
+            if name == "rayane":
+                times = times + 1
 
-            if times == 1:
-                io.output(red, 0)
-                io.output(green, 1)
-                p_buzzer.start(90)
-                p_servo.start(12.5)
-                time.sleep(5)
-                p_buzzer.stop()
+            #if recognized five times turn on green led and the servo motor and buzzer
+            if times == 5:
+                #io.output(red, 0)
+                #io.output(green, 1)
+                #p_servo.start(12.5)
+                #time.sleep(5)
                 done = True
 
-        #cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-    #cv2.imshow('img', img)
+        #draw a rectangle around the face
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    
+    #show the image
+    cv2.imshow('img', img)
+    
+    #break out of loop on keystroke
     k = cv2.waitKey(30) & 0xFF
     if k == 27:
         break
+    
+    #delete previous snapshot in order to get the next snapshot to prevent buffer overflow
+    rawCapture.truncate(0)
 
-cap.release()
+#close the window that shows the image
 cv2.destroyAllWindows()
+
+#turn the servo motor back to original position
 p_servo.ChangeDutyCycle(2.5)
 time.sleep(1)
 p_servo.stop()
+
+#clean IO
 io.cleanup()
--- push_button.py
+++ push_button.py
@@ -3,13 +3,10 @@ import RPi.GPIO as io
 
 io.setmode(io.BOARD)
 io.setup(18, io.IN, pull_up_down=io.PUD_DOWN)
-io.setup(7, io.OUT)
-io.output(7, 0)
 
 try:
 	while True:
 		if io.input(18):
 			os.system("/home/pi/.virtualenvs/cv/bin/python identification.py")
-			print("called from push button")
 except KeyboardInterrupt:
 	io.cleanup()
